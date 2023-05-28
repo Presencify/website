aacdbb895be8d8f92128a98c50fb00791f5a7bf2
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-05-28 08:19:14 -0300

    build: add project structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+import "./globals.css"
+import { Commissioner } from "next/font/google"
+
+const inter = Commissioner({ subsets: ["latin"] })
+
+export const metadata = {
+  title: "Presencify",
+  description: "Show your browser activity on your discord presence with Presencify"
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
