801cb0f64ac7e2fd6e3c474395d0acb2243bcef1
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-05-28 13:59:20 -0300

    build: add "svgr" dependency

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+webpack(config, { dev: isDev, isServer }) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      resourceQuery: /svgr/,
+      use: ["@svgr/webpack"],
+    });
+    config.module.rules.push({
+      test: /\.svg$/i,
+      loader: "next-image-loader",
+      issuer: { not: /\.(css|scss|sass)$/ },
+      dependency: { not: ["url"] },
+      resourceQuery: { not: [/svgr/] },
+      options: { isServer, isDev, basePath: "", assetPrefix: "" },
+    });
+  return config
+}
+}
 
 module.exports = nextConfig
