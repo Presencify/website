46232c6e57a4ea29a64573a6330c9e18df94523a
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-05-28 14:41:07 -0300

    fix: fix @svgr/webpack configuration

--- next.config.js
+++ next.config.js
@@ -1,22 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-webpack(config, { dev: isDev, isServer }) {
+  reactStrictMode: true,
+   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      resourceQuery: /svgr/,
-      use: ["@svgr/webpack"],
+      use: ['@svgr/webpack'],
     });
-    config.module.rules.push({
-      test: /\.svg$/i,
-      loader: "next-image-loader",
-      issuer: { not: /\.(css|scss|sass)$/ },
-      dependency: { not: ["url"] },
-      resourceQuery: { not: [/svgr/] },
-      options: { isServer, isDev, basePath: "", assetPrefix: "" },
-    });
-  return config
-}
+    return config;
+  },
 }
 
 module.exports = nextConfig
