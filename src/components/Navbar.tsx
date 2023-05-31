d3c9f7411564a5b7c8ce5f415039b5156ee1cc2f
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-05-31 15:02:24 -0300

    feat(Navbar): add active section on navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,30 @@
-import { SectionsEnum } from "@/enums.d"
+"use client"
+import Link from "next/link"
+import { useEffect, type FC, useState } from "react"
+import { useSearchParams } from "next/navigation"
 import type { NavbarProps } from "@/interfaces.d"
-import type { FC } from "react"
+import { SectionsEnum } from "@/enums.d"
 
 const Navbar: FC<NavbarProps> = ({ className }) => {
+  const [activeSection, setActiveSection] = useState("/")
+  const searchParams = useSearchParams()
+
+  useEffect(() => {
+    const pathname = window.location.hash.substring(1)
+    setActiveSection(pathname)
+  }, [searchParams])
+
   return (
     <nav>
       <ul className={`flex gap-16 ${className}`}>
         {Object.values(SectionsEnum).map((section, index) => (
-          <li key={index} className="hover:text-green-1">
-            <a href={`#${section}`} className="text-base lg:text-lg">
+          <li
+            key={index}
+            className={`hover:text-green-1 ${activeSection === section ? "text-green-1" : ""}`}
+          >
+            <Link href={`#${section}`} className="text-base lg:text-lg">
               {section}
-            </a>
+            </Link>
           </li>
         ))}
       </ul>
