59fe1aae3e4a43c0a2d27b4fd86a0a8121703d55
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-06-02 14:46:19 -0300

    feat: update "presencify" file version

--- src/utils/handleDownload.ts
+++ src/utils/handleDownload.ts
@@ -1,8 +1,8 @@
 export const handleDownload = () => {
-  const downloadUrl = "/Precensify.zip" // Ruta relativa al archivo .rar en la carpeta 'public'
+  const downloadUrl = "/Presencify.v0.0.2.zip" // Ruta relativa al archivo .rar en la carpeta 'public'
 
   const link = document.createElement("a")
   link.href = downloadUrl
-  link.download = "Precensify.zip" // Nombre que se utilizará para guardar el archivo
+  link.download = "Presencify.v0.0.2.zip" // Nombre que se utilizará para guardar el archivo
   link.click()
 }
